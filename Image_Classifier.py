9c7f4d3d3d5cb7aa664d76006ca710f378914656
ery010 <49173353+ery010@users.noreply.github.com>
2020-01-01 21:27:45 -0800

    Updated Notebook and py

--- Image_Classifier.py
+++ Image_Classifier.py
@@ -1,27 +1,35 @@
 import os
+from skimage import io
 from skimage.io import imread
-from skimage.transform import rescale
-import numpy as np
-import pandas as pd
-import tensorflow as tf
-from sklearn.pipeline import Pipeline
-import skimage
-import pickle
+from skimage.transform import resize
+import time
+import sys
+from matplotlib import pyplot as plt
 
-# Data pre-processing
 
-train_data_list = []
-count = 0
-train_dir = os.path.join('data', 'cars_train')
-for category in os.listdir(train_dir):
-    path = os.path.join(train_dir, category)
-    for car_img in os.listdir(path):
-        img_arr = imread(os.path.join(path, car_img))
-        rescaled_img = rescale(img_arr, 0.5)
-        train_data_list.append([rescaled_img, count])
-    count += 1
+''' Baseline Model '''
 
-print(train_data_list[0])
+
+def create_training_data(resize_factor):    # Preparing training data
+    train_data_list = []
+    idx = 0
+    train_dir = os.path.join('data', 'cars_train')
+    for category in os.listdir(train_dir):
+        path = os.path.join(train_dir, category)
+        for car_img in os.listdir(path):
+            try:
+                img_arr = imread(os.path.join(path, car_img))
+                resized_img = resize(img_arr, (img_arr.shape[0]//resize_factor, img_arr.shape[1]//resize_factor))
+                train_data_list.append((resized_img, idx))
+            except Exception as e:
+                pass
+        idx += 1
+    return train_data_list
+
+
+SIZE = 4
+train_data = create_training_data(SIZE)
+
 
 
 
